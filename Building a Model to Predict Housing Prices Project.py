6f38c054242f67d2f8cf3a5aa93855bc2f6754f7
jennIT20 <87151489+jennIT20@users.noreply.github.com>
2024-07-02 22:56:13 -0400

    Update Building a Model to Predict Housing Prices Project.py

--- Building a Model to Predict Housing Prices Project.py
+++ Building a Model to Predict Housing Prices Project.py
@@ -13,10 +13,6 @@ train.head()
 # investigate Sale Price
 train['SalePrice']
 
-"""To execute the code in the above cell, select it with a click and then either press the play button to the left of the code, or use the keyboard shortcut "Command/Ctrl+Enter". To edit the code, just click the cell and start editing.
-
-Variables that you define in one cell can later be used in other cells:
-"""
 
 plt.hist(train['SalePrice'])
 
@@ -35,7 +31,6 @@ Y = train ['SalePrice']
 X = X.drop(['SalePrice'], axis = 1)
 X
 
-"""You can import your own data into Colab notebooks from your Google Drive account, including from spreadsheets, as well as from Github and many other sources. To learn more about importing data, and how Colab can be used for data science, see the links below under [Working with Data](#working-with-data)."""
 
 # build Linear Regression Model
 from sklearn.linear_model import LinearRegression
